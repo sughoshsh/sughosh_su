79eb647ed6001cfd17b6278d0622f49d8eb4bb30
sughoshsh <sughoshsh@outlook.com>
2024-08-12 07:54:25 +0530

    botwithazuredb
    connecting bot with azure db to read data from table.

---
+++ 02.echo-bot/Models/Address.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EchoBot.Models;
+
+public partial class Address
+{
+    public int AddressId { get; set; }
+
+    public string AddressLine1 { get; set; }
+
+    public string AddressLine2 { get; set; }
+
+    public string City { get; set; }
+
+    public string StateProvince { get; set; }
+
+    public string CountryRegion { get; set; }
+
+    public string PostalCode { get; set; }
+
+    public Guid Rowguid { get; set; }
+
+    public DateTime ModifiedDate { get; set; }
+
+    public virtual ICollection<CustomerAddress> CustomerAddresses { get; set; } = new List<CustomerAddress>();
+
+    public virtual ICollection<SalesOrderHeader> SalesOrderHeaderBillToAddresses { get; set; } = new List<SalesOrderHeader>();
+
+    public virtual ICollection<SalesOrderHeader> SalesOrderHeaderShipToAddresses { get; set; } = new List<SalesOrderHeader>();
+}
---
+++ 02.echo-bot/Program.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
+using Azure.Security.KeyVault.Secrets;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+//            .ConfigureAppConfiguration((context, config) =>
+//{
+//    var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+//    config.AddAzureKeyVault(keyVaultEndpoint, new VisualStudioCredential());
+//})
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureLogging((logging) =>
+                    {
+                        logging.AddDebug();
+                        logging.AddConsole();
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
