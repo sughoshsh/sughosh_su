36485260dc67a6275cde1473a8fbc870310c8d75
sughoshsh <sughoshsh@outlook.com>
2024-08-15 13:15:31 +0530

    coreBotWithDBnQnA
    AZure DB with qnA maker

---
+++ 13.core-bot/Dialogs/AzureSQLDBDialog.cs
@@ -0,0 +1,98 @@
+﻿using CoreBot.Models;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using CoreBot.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreBot.Dialogs
+{
+    public class AzureSQLDBDialog : CancelAndHelpDialog
+    {
+        FinacleSqldbContext context;
+        public FinacleSqldbContext Context { get { return context; } }
+
+        public AzureSQLDBDialog()
+            : base(nameof(AzureSQLDBDialog))
+        {
+            context = new FinacleSqldbContext();
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new ChoicePrompt(nameof(ChoicePrompt)));
+            AddDialog(new ConfirmPrompt(nameof(ConfirmPrompt)));
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+                IntroStepAsync,
+                ActStepAsync,
+                FinalStepAsync,
+            }));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> IntroStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions
+            {
+                Prompt = MessageFactory.Text("Please enter the region Code.")
+            }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ActStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["Name"] = (string)stepContext.Result;
+
+            // Do some query on SQL Database
+            var RegName = (string)stepContext.Values["Name"];
+            Region region = FetchRegionName(RegName);
+
+            if (region == null)
+            {
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Region with id {RegName} not found."), cancellationToken);
+            }
+            else
+            {
+                var replyText = region.Name + ": " + region.DisplayName;
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            }
+
+
+            return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions
+            {
+                Prompt = MessageFactory.Text("Would you like to search for more region names?")
+            }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            if ((bool)stepContext.Result)
+            {
+                return await stepContext.ReplaceDialogAsync(InitialDialogId, null, cancellationToken);
+            }
+            else
+            {
+                return await stepContext.EndDialogAsync(null, cancellationToken);
+            }
+        }
+
+        public Region FetchRegionName(string str)
+        {
+            Region region;
+            try
+            {
+                region = (from e in Context.Regions
+                            where e.Name == str
+                            select e).FirstOrDefault();//Query for employee details with id
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return region;
+        }
+    }
+}
---
+++ 13.core-bot/Dialogs/MainDialog.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.13.2
+
+using AdaptiveCards;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Http;
+using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreBot.Dialogs
+{
+    
+    public class MainDialog : ComponentDialog
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _httpClientFactory;
+        protected readonly ILogger Logger;
+
+        // Dependency injection uses this constructor to instantiate MainDialog
+        public MainDialog(ILogger<MainDialog> logger, IConfiguration configuration, IHttpClientFactory httpClientFactory)
+            : base(nameof(MainDialog))
+        {
+
+            Logger = logger;
+            _configuration = configuration;
+            _httpClientFactory = httpClientFactory;
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new ChoicePrompt(nameof(ChoicePrompt)));
+            AddDialog(new AzureSQLDBDialog());
+            AddDialog(new QnAMakerDialog(configuration, httpClientFactory));
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+                IntroStepAsync,
+                ActStepAsync,
+                FinalStepAsync,
+            }));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> IntroStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text("Please choose an option to proceed further."), cancellationToken);
+            List<string> operationList = new List<string> { "Use Azure SQL DB", "Use QnA Maker" };
+            // Create card
+            var card = new AdaptiveCard(new AdaptiveSchemaVersion(1, 0))
+            {
+                // Use LINQ to turn the choices into submit actions
+                Actions = operationList.Select(choice => new AdaptiveSubmitAction
+                {
+                    Title = choice,
+                    Data = choice,  // This will be a string
+                }).ToList<AdaptiveAction>(),
+            };
+            // Prompt
+            return await stepContext.PromptAsync(nameof(ChoicePrompt), new PromptOptions
+            {
+                Prompt = (Activity)MessageFactory.Attachment(new Attachment
+                {
+                    ContentType = AdaptiveCard.ContentType,
+                    // Convert the AdaptiveCard to a JObject
+                    Content = JObject.FromObject(card),
+                }),
+                Choices = ChoiceFactory.ToChoices(operationList),
+                // Don't render the choices outside the card
+                Style = ListStyle.None,
+            },
+                cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ActStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["UserOperation"] = ((FoundChoice)stepContext.Result).Value;
+
+            string operation = (string)stepContext.Values["UserOperation"];
+            if (operation.Equals("Use Azure SQL DB"))
+            {
+                return await stepContext.BeginDialogAsync(nameof(AzureSQLDBDialog), null, cancellationToken);
+            }
+            else
+            {
+                return await stepContext.BeginDialogAsync(nameof(QnAMakerDialog), null, cancellationToken);
+            }
+        }
+
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+
+            // Restart the main dialog with a different message the second time around
+            var promptMessage = "What else can I do for you?";
+            return await stepContext.ReplaceDialogAsync(InitialDialogId, promptMessage, cancellationToken);
+        }
+    }
+}
